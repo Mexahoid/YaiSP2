36a37cce62f2c40787cce91f4dc5954e07f760f1
Mexahoid <mirramax97@Mail.ru>
2017-06-07 21:12:47 +0300

    Добавьте файлы проекта.

---
+++ YaiSP2/Logics/CashBox.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading;
+
+namespace YaispTwoEight
+{
+    public class CashBox
+    {
+        private Collections.MoneyList MoneyBox;
+        private bool Active;
+        private int[] MoneyStacksCount;
+
+        public CashBox()
+        {
+            MoneyBox = new Collections.MoneyList();
+            Active = false;
+        }
+        public void FillMoney(int[] Nom)
+        {
+            MoneyBox = new Collections.MoneyList();
+            MoneyBox.FillMoney(Nom);
+        }
+        public int GetMoneyCount()
+        {
+            return MoneyBox.ReturnAllCashCount();
+        }
+        public int[,] GetMoneyArray()
+        {
+            return MoneyBox.GetMoneyArray();
+        }
+        public void Deactivate()
+        {
+            Active = false;
+        }
+        public void Activate()
+        {
+            Active = true;
+        }
+        public bool GetActiveState()
+        {
+            return Active;
+        }
+        public bool OrderMoneySum(int Value)
+        {   //Коды возврата нельзя выдать, вообще не хватает
+            MoneyStacksCount = MoneyBox.ReturnMoneyStackCount();
+            int[] DesireStacks = GetValueToNoteConversion(Value);
+            int L = MainUnitProcessor.GlobalNominals.Length;
+            bool IsAvailable = true;
+            for (int i = 0; i < L; i++)
+            {
+                if (MoneyStacksCount[i] < DesireStacks[i])
+                {
+                    IsAvailable = false;
+                    if (i != L - 1)  //Если i четное - то в следующем номинале добавляем 5, иначе 2
+                    {
+                        DesireStacks[i + 1] += (i % 2 == 0 || i == 0) ?
+                            (5 * (-MoneyStacksCount[i] + DesireStacks[i])) :
+                            (2 * (-MoneyStacksCount[i] + DesireStacks[i]));
+                        DesireStacks[i] = MoneyStacksCount[i];
+                    }
+                    else
+                        break;
+                }
+                else
+                    IsAvailable = true;
+            }
+            if (IsAvailable)
+            {
+                Collections.MoneyStack CurrentStack;
+                for (int i = 0; i < L; i++)
+                {
+                    CurrentStack = MoneyBox.ReturnByIndex(i);
+                    CurrentStack.TakeMoneyFromStack(DesireStacks[i]);
+                }
+                return true;
+            }
+            return false;
+        }
+        private int[] GetValueToNoteConversion(int Value)
+        {
+            int L = MainUnitProcessor.GlobalNominals.Length;
+            int[] Counts = new int[L];
+            for (int i = 0; i < L; i++)
+            {
+                Counts[i] = Value / MainUnitProcessor.GlobalNominals[i];
+                Value %= MainUnitProcessor.GlobalNominals[i];
+            }
+            return Counts;
+        }
+    }
+}
---
+++ YaiSP2/Logics/DrawingClass.cs
@@ -0,0 +1,123 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+
+namespace YaispTwoEight
+{
+  public static class DrawingClass
+  {
+    public static Graphics PictureBoxCanvas;
+    public static Graphics LogicsCanvas;
+    public static Bitmap ImageCanvas;
+
+    public static void GetImage()
+    {
+      DrawCashBox();
+      DrawOutlaw();
+      DrawQueue();
+    }
+    public static void DrawCashBox()
+    {
+      LogicsCanvas.DrawLine(Pens.Black, 0, 465, 740, 465);
+      Point[] Points = new Point[5] {
+        new Point(60, 170),
+        new Point(125, 170),
+        new Point(170, 295),
+      new Point(170, 465),
+      new Point(60, 465)};
+      LogicsCanvas.FillPolygon(Brushes.LightGray, Points);
+      LogicsCanvas.DrawPolygon(Pens.Black, Points);
+      LogicsCanvas.DrawString("Всего денег на сумму: " + MainUnitProcessor.GetMoneyCount().ToString() + " p.", new Font("Arial", 20, FontStyle.Bold), Brushes.DarkBlue, 50, 50);
+    }
+    public static void MainDrawMethod()
+    {
+      LogicsCanvas.Clear(Color.White);
+      GetImage();
+      PictureBoxCanvas.DrawImage(ImageCanvas, 0, 0);
+    }
+    private static void DrawOutlaw()
+    {
+      if (MainUnitProcessor.Outlaw != null)
+        DrawWithParameters(MainUnitProcessor.Outlaw.GetParameters());
+    }
+    private static void DrawQueue()
+    {
+      if (MainUnitProcessor.Queue != null)
+      {
+        List<int[]> Queue = MainUnitProcessor.Queue.ReDrawQueue();
+        foreach (int[] Arr in Queue)
+          DrawWithParameters(Arr);
+      }
+    }
+    private static void DrawWithParameters(int[] Arr)
+    {
+      int X, Y, Shirt, Pants, State, Moving, Desire;
+      X = Arr[0];
+      Y = Arr[1];
+      Shirt = Arr[2];
+      Pants = Arr[3];
+      State = Arr[4];
+      Moving = Arr[5];
+      Desire = Arr[6];
+      LogicsCanvas.FillRectangle(DrawingClass.GetColor(Shirt), X, Y, 40, 100);
+      LogicsCanvas.DrawRectangle(Pens.Black, X, Y, 40, 100);
+      LogicsCanvas.DrawRectangle(Pens.Black, X + 10, Y + 15, 20, 80);
+      LogicsCanvas.FillRectangle(DrawingClass.GetColor(Pants), X + 5, Y + 100, 30, 100);
+      LogicsCanvas.DrawRectangle(Pens.Black, X + 5, Y + 100, 30, 100);
+      LogicsCanvas.DrawEllipse(Pens.Black, X + 5, Y - 30, 30, 30);
+      Rectangle Rect = new Rectangle(X - 5, Y - 100, 50, 50);
+      switch (State)
+      {
+        case 1:  //Только идет
+          LogicsCanvas.FillEllipse(Brushes.Yellow, Rect);
+          LogicsCanvas.FillRectangle(Brushes.Black, X + 10, Y - 65, 20, 3);
+          break;
+        case 2:  //Получил
+          LogicsCanvas.FillEllipse(Brushes.LightGreen, Rect);
+          LogicsCanvas.FillClosedCurve(Brushes.Black, new Point[6] {
+                new Point(X + 10, Y - 68),
+                new Point(X + 20, Y - 65),
+                new Point(X + 30, Y - 68),
+                new Point(X + 30, Y - 65),
+                new Point(X + 20, Y - 62),
+                new Point(X + 10, Y - 65) });
+          break;
+        case 3:  //Не получил
+          LogicsCanvas.FillEllipse(Brushes.Red, Rect);
+          LogicsCanvas.FillClosedCurve(Brushes.Black, new Point[6] {
+                new Point(X + 10, Y - 65),
+                new Point(X + 20, Y - 68),
+                new Point(X + 30, Y - 65),
+                new Point(X + 30, Y - 62),
+                new Point(X + 20, Y - 65),
+                new Point(X + 10, Y - 62) });
+          break;
+      }
+      if (Moving == 3)
+        LogicsCanvas.DrawEllipse(Pens.Black, X + 25, Y - 20, 4, 4);
+      else
+        LogicsCanvas.DrawEllipse(Pens.Black, X + 10, Y - 20, 4, 4);
+      LogicsCanvas.FillEllipse(Brushes.Black, X + 5, Y - 85, 10, 10);
+      LogicsCanvas.FillEllipse(Brushes.Black, X + 25, Y - 85, 10, 10);
+      LogicsCanvas.DrawString(Desire.ToString() + " p.", new Font("Arial", 10), Brushes.Black, X + 5, Y - 50);
+      LogicsCanvas.DrawEllipse(Pens.Black, Rect);
+    }
+    public static Brush GetColor(int type)
+    {
+      switch (type)
+      {
+        case 10:
+          return Brushes.Olive;
+        case 50:
+          return Brushes.LightBlue;
+        case 100:
+          return Brushes.DarkGoldenrod;
+        case 500:
+          return Brushes.Violet;
+        case 1000:
+          return Brushes.Cyan;
+        default: //5000
+          return Brushes.DarkOrange;
+      }
+    }
+  }
+}
